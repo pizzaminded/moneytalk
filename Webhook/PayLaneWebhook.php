9b89026ca00cf7ec01e636d4c077d79a520873a1
pizzaminded <pizzaminded@users.noreply.github.com>
2017-07-05 00:49:54 +0200

    added webhook for PayLane

--- Webhook/PayLaneWebhook.php
+++ Webhook/PayLaneWebhook.php
@@ -3,8 +3,10 @@
 namespace pizzaminded\MoneyTalkBundle\Webhook;
 
 use pizzaminded\MoneyTalkBundle\Entity\PayLane;
+use pizzaminded\MoneyTalkBundle\Exception\ParserException;
 use pizzaminded\MoneyTalkBundle\MoneyTalkableInterface;
 use pizzaminded\MoneyTalkBundle\Payment\PaymentProcessor;
+use Symfony\Component\HttpFoundation\ParameterBag;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -30,22 +32,61 @@ class PayLaneWebhook implements WebhookInterface
     protected $entity;
 
     /**
+     * @var bool
+     */
+    protected $isParsed;
+
+    /**
      * PayLaneWebhook constructor.
      */
     public function __construct()
     {
         $this->entity = new PayLane();
+        $this->isParsed = false;
     }
 
     /**
      * @param Request $request
      * @return $this
+     * @throws ParserException
      */
     public function parseRequest(Request $request)
     {
         $this->request = $request;
 
+        if ($this->method === null) {
+            throw new ParserException('Method is not set.');
+        }
 
+        /** @var $parameterBag ParameterBag */
+        if (strtoupper($this->method) === 'GET') {
+            $parameterBag = $this->request->query;
+        } elseif (strtoupper($this->method) === 'POST') {
+            $parameterBag = $this->request->request;
+        }
+
+        //if these fields are missing, we can assume that is not a correct request
+        if(!($parameterBag->has('status') && $parameterBag->has('hash'))) {
+            throw new ParserException('Unable to parse request.');
+        }
+
+        $status = $parameterBag->get('status');
+        $currency = $parameterBag->get('currency');
+        $description = $parameterBag->get('description');
+        $amount = $parameterBag->get('amount');
+        $hash = $parameterBag->get('hash');
+        $idSale = $parameterBag->get('id_sale');
+        $idError = $parameterBag->get('id_error');
+
+        $this->entity->setStatus($status)
+            ->setCurrency($currency)
+            ->setDescription($description)
+            ->setAmount($amount)
+            ->setHash($hash)
+            ->setIdSale($idSale)
+            ->setIdError($idError);
+
+        $this->isParsed = true;
 
         return $this;
     }
@@ -56,9 +97,9 @@ class PayLaneWebhook implements WebhookInterface
      */
     public function setMethod($method)
     {
-       $this->method = strtoupper($method);
+        $this->method = strtoupper($method);
 
-       return $this;
+        return $this;
     }
 
     /**
@@ -71,9 +112,15 @@ class PayLaneWebhook implements WebhookInterface
 
     /**
      * @return PaymentProcessor
+     * @throws \pizzaminded\MoneyTalkBundle\Exception\ParserException
      */
     public function getPaymentProcessor(): PaymentProcessor
     {
-        // TODO: Implement getPaymentProcessor() method.
+        if($this->isParsed) {
+            return new PaymentProcessor($this->entity);
+        }
+
+        throw new ParserException('You can\'t invoke a payment processor if payment is not processed');
     }
+
 }
