110460c320722948cca182e52b4e70bdbf8f498b
pizzaminded <pizzaminded@users.noreply.github.com>
2017-07-04 20:12:18 +0200

    added interface for webhooks

---
+++ Webhook/WebhookInterface.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace pizzaminded\MoneyTalkBundle\Webhook;
+
+
+use pizzaminded\MoneytalkBundle\MoneytalkableInterface;
+
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Interface WebhookInterface
+ * @package pizzaminded\MoneyTalkBundle\Webhook
+ * @author pizzaminded <github.com/pizzaminded>
+ */
+interface WebhookInterface
+{
+    /**
+     * @param string $method
+     * @return $this
+     */
+    public function setMethod($method);
+
+    /**
+     * @param Request $request
+     * @return $this
+     */
+    public function parseRequest(Request $request);
+
+
+    /**
+     * @return MoneytalkableInterface
+     */
+    public function getEntity(): MoneytalkableInterface;
+}
