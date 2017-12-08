b48c5c32803167c6a7679b0bece6576567f807e9
pizzaminded <pizzaminded@users.noreply.github.com>
2017-12-08 02:12:29 +0100

    making package from bundle: deprecated main bundle class

--- pizzamindedMoneyTalkBundle.php
+++ pizzamindedMoneyTalkBundle.php
@@ -4,6 +4,13 @@ namespace pizzaminded\MoneyTalkBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class pizzamindedMoneyTalkBundle
+ * @package pizzaminded\MoneyTalkBundle
+ * @author pizzaminded <miki@appvende.net>
+ * @license MIT
+ * @deprecated
+ */
 class pizzamindedMoneyTalkBundle extends Bundle
 {
 }
